8465ef0d6ad9ecd9d23fb44c1c2bc7fd3e5434a8
zhangyunqiang2019 <2644888555@qq.com>
2019-08-14 08:44:27 +0800

    系统主管理js

---
+++ PropertyManagement06/src/main/resources/static/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * 系统主管理JS
+ * @Author:张云强
+ */
+$.jgrid.defaults.styleUI = 'Bootstrap';
+$(function(){
+	
+	//点击左面功能菜单处理
+	$("ul.sidebar-menu li.treeview  ul li a").on("click",function(event){
+		var url=$(this).attr("href");
+		$("section#maincontent").load(url); //载入对应的对象管理主页
+		event.preventDefault();  //停止对象的默认行为。
+	});
+	
+});

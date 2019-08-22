4cfb24201637c3649a50282f07f6ca894432d6fb
liuyongchao <1793295857@qq.com>
2019-08-22 14:31:00 +0800

    增加jquery3版本

--- PropertyManagement06/src/main/resources/static/index.js
+++ PropertyManagement06/src/main/resources/static/index.js
@@ -3,9 +3,11 @@
  * @Author:刘勇超
  */
 $(function(){
-	
-	$("ul.sidebar-menu li.treeview ul li a ").on("click"),function(event){
+	$("ul.sidebar-menu li.treeview  ul li a").on("click",function(event){
+		
 		alert("OK");
+		
+		var url=$(this).attr("href");
+		$("section#maincontent").load(url); //载入对应的对象管理主页
 	});
-	
 });
